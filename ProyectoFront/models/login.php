35b2d8c1db9ebbefed8cb3e551409012bf178643
aodvmake <61640456+aodvmake@users.noreply.github.com>
2021-05-11 12:16:12 -0500

    Modulo para agregar empresas
    Wireframe

--- ProyectoFront/models/login.php
+++ ProyectoFront/models/login.php
@@ -5,7 +5,7 @@ class modeloguardar{
        function guardar($email,$pass){
        $cclonn = new call();
        $rrcnx = $cclonn->callbd();
-     //Inserta en las tablas necesarias 
+     //Selecciona las tablas necesarias 
       $conp=mysqli_query($rrcnx,"SELECT US.IDusuario,US.email,US.pass,puesto.nombre FROM usuario AS US INNER JOIN puesto 
       ON US.IDpuesto=puesto.IDpuesto WHERE US.email='$email' AND US.pass='$pass' ");
       $rows="";
